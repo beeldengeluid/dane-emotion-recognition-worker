2cca10b040f3cc66152874b45ff3c3d7fbe4f26e
Kathleendeboer <k.k.deboer@students.uu.nl>
2024-03-04 15:05:16 +0100

    Update sseft_shap_new.py
    Debugged untill my colab resources hit their limit. It is not fully running but it comes close. Mostly i fixed an issue with the access of the original input (which i could not give as an argument, while this is not compatible with shap) I made these global.

--- sseft_shap_new.py
+++ sseft_shap_new.py
@@ -13,49 +13,51 @@ from fairseq import checkpoint_utils, options, progress_bar, utils
 ## This file contains the explainability framework merged with the model 'sse-ft', it is a work in progress, however the masking logic is tested and is correct
 
 ## Main for now parses the args from the command prompt and passes them to load_models()
-def main():
-    print("in main")
-    parser = options.get_validation_parser()
-    args = options.parse_args_and_arch(parser)
 
-    # only override args that are explicitly given on the command line
-    override_parser = options.get_validation_parser()
-    override_args = options.parse_args_and_arch(override_parser, suppress_defaults=True)
 
-    load_models(args)
-    utils.import_user_module(args)
-    use_fp16 = args.fp16
-    use_cuda = torch.cuda.is_available() and not args.cpu
-    load_models(args)
 
-if __name__ == '__main__':
-    main()
 
 # Load_models() gets the model from the fairseq library and sets the checkpoint, it initialises a data iterator and loops over the samples in the dataset. 
 # For now I need to test the masking functions together with the model, later i will adjust the code so that it can handle data from Sound and Vision
-
-
-
+global text_tensor
+global audio_tensor
+global video_tensor
+global text_length
+global audio_length
+global padded_amount_value
+global samples_per_token
+global task
+global model
+global criterion
+global use_cuda
 def load_models(args, override_args=None):
+    global task
+    global model
+    global criterion
+    global use_cuda
+    utils.import_user_module(args)
+    use_fp16 = args.fp16
+    use_cuda = torch.cuda.is_available() and not args.cpu
+
     if override_args is not None:
             overrides = vars(override_args)
             overrides.update(eval(getattr(override_args, 'model_overrides', '{}')))
-        else:
-            overrides = None
-        ## Load ensemble and extract model
-        print('| loading model(s) from {}'.format(args.path))
-        models, model_args, task = checkpoint_utils.load_model_ensemble_and_task(
-            [args.path],
-            arg_overrides=overrides,
+    else:
+        overrides = None
+    ## Load ensemble and extract model
+    print('| loading model(s) from {}'.format(args.path))
+    models, model_args, task = checkpoint_utils.load_model_ensemble_and_task(
+        [args.path],
+        arg_overrides=overrides,
         )
-        model = models[0]
+    model = models[0]
 
         ## Move models to GPU
-        for model in models:
-            if use_fp16:
-                model.half()
-            if use_cuda:
-                model.cuda()
+    for model in models:
+        if use_fp16:
+            model.half()
+        if use_cuda:
+            model.cuda()
     print(model_args)
 
     ## Build criterion
@@ -102,6 +104,13 @@ def load_models(args, override_args=None):
         #text_tensor = torch.randn(1, 30)  # Example dimensions (batch_size, seq_length, embedding_size)
         #video_tensor = torch.randn(1, 3, 300, 256, 256)  # Example dimensions (batch_size, channels, frames, height, width)
         #padded_amount_value = 100
+        global audio_tensor
+        global text_tensor
+        global video_tensor
+        global padded_amount_value
+        global text_length
+        global audio_length
+        global samples_per_token
         audio_tensor = sample['net_input']['audio']
         text_tensor = sample['net_input']['text']
         video_tensor = sample['net_input']['video']
@@ -112,23 +121,26 @@ def load_models(args, override_args=None):
         last_non_zero_index = np.max(np.nonzero(text))
         text_length  = len(text[:last_non_zero_index + 1])
         text_tokens_ids = torch.tensor(range(1, text_length+1)).unsqueeze(0)
+        #text_input = TextInputData(input_ids=text_tokens_ids, text_length=text_length, text_tensor = text_tensor)
+        ## Audio tokens, each token represents a segment of a second in spectogram values (the sanmple rate is used to calculate the amount of values per token)
         audio = audio_tensor.squeeze(0).cpu().numpy()
         last_index = len(audio) - np.argmax(np.flip(audio) != 1) - 1
-
-        ## Audio tokens, each token represents a segment of a second in spectogram values (the sanmple rate is used to calculate the amount of values per token)
+        print(audio.shape)
         audio = audio[:last_index + 1]
+        print(audio.shape)
         sample_rate = 16000
         sample_duration = 1.0 / sample_rate
         token_duration = 1.0
         samples_per_token = int(token_duration / sample_duration)
         audio_length = math.ceil((len(audio)/samples_per_token))
         audio_tokens_ids = torch.tensor(range(1, audio_length+1)).unsqueeze(0) 
-
+        #audio_input = AudioInputData(input_ids=audio_tokens_ids, audio_length=audio_length, audio_tensor = audio_tensor)
+        print(audio_tokens_ids.shape)
         ## Video tokens, each pixel is represented as a token (256 x 256 is an image) 
         video_token_ids = torch.tensor(range(1, 257)).unsqueeze(0)
-
+        #video_input = VideoInputData(input_ids=video_token_ids, video_tensor = video_tensor)
         ## All tokens are concatenated together
-        All_token_ids = torch.cat((text_tokens_ids, audio_tokens_ids, video_token_ids, ), 1).unsqueeze(1)
+        All_token_ids = torch.cat((text_tokens_ids, audio_tokens_ids, video_token_ids), 1).unsqueeze(1)
         print(All_token_ids)
 
         ## A SHAP explainer object is made with the custom masker and the model prediction function
@@ -152,38 +164,58 @@ def custom_masker(mask, x):
 ## From the masked tokens, based on the token generaton logic, the input is reconstructed to the form needed by the model
 def get_model_prediction(x):
     with torch.no_grad():
-        masked_text_tokens_ids = torch.tensor(x[:, :, :text_length]) 
-        masked_audio_tokens_ids = torch.tensor(x[:, :, text_length: text_length + audio_length])
-        masked_video_tokens_ids = torch.tensor(x[:, :, text_length + audio_length:])
+        print(x)
+        print(x.shape)
+        masked_text_tokens_ids = torch.tensor(x[:, :text_length]) 
+        print(masked_text_tokens_ids)
+        masked_audio_tokens_ids = torch.tensor(x[:, text_length: text_length + audio_length])
+        masked_video_tokens_ids = torch.tensor(x[:, text_length + audio_length:])
         result = np.zeros(x.shape[0])
         for i in range(x.shape[0]):
-            masked_text_tensor = text_tensor.copy()
+            masked_text_tensor = text_tensor.clone()
             for k in range(1, len(masked_text_tokens_ids)):
-                if masked_text_tokens_ids[k] == 0:
-                    masked_text_tensor[k] = 0 ## Here the real text masking happens
-            masked_audio_tensor = audio_tensor.copy()
-            for k in range(0, len(masked_audio_tokens_ids)):
-                if audio_tokens_ids[k] == 0:
+                if masked_text_tokens_ids[0,k].item() == 0:
+                    masked_text_tensor[0,k] = 0 ## Here the real text masking happens
+            masked_audio_tensor = audio_tensor.clone()
+            print(len(masked_audio_tokens_ids))
+            for k in range(masked_audio_tokens_ids.size(1)-1):
+                print(k)
+                if masked_audio_tokens_ids[0,k].item() == 0:
                     masked_audio_tensor[k:k+samples_per_token] = 0 ## Here the real audio masking happens
-                        masked_video_tensor = video_tensor.clone()
+            masked_video_tensor = video_tensor.clone()
             num_patches = 256
             patch_size = 1
             num_frames = 300
             print(masked_video_tensor)
-            for k in range(0, len(masked_video_tokens_ids)):
+            print(masked_video_tensor.shape)
+            for k in range(masked_video_tokens_ids.size(1)-1):
                 print(masked_video_tokens_ids)
-                if masked_video_tokens_ids[0,0,k] == 0 :
+                if masked_video_tokens_ids[0,k].item() == 0 :
                     start_row = (k // (256 // patch_size)) * patch_size
                     start_col = (k % (256 // patch_size)) * patch_size
                     for frame_idx in range(masked_video_tensor.shape[2]):
                       if torch.any(masked_video_tensor[0, :, frame_idx] == -1):
                         continue  # Skip padded frames
-                      masked_video_tensor[:, :, frame_idx, start_row:start_row+patch_size, start_col:start_col+patch_size] = 0 ## Here the real video masking happens
+                      masked_video_tensor[:,:, frame_idx, start_row:start_row+patch_size, start_col:start_col+patch_size] = 0 ## Here the real video masking happens
             ## Input in the right shape fir model
-            masked_input = {'net_input': {'audio': masked_audio_tensor, 'text': masked_text_tensor, 'video': masked_video_tensor, 'padded_amount':padded_amount_value }}
-            masked_input = utils.move_to_cuda(masked_input) if use_cuda else masked_input
+            #masked_input = {'net_input': {'audio': masked_audio_tensor, 'text': masked_text_tensor, 'video': masked_video_tensor, 'padded_amount':padded_amount_value }}
+            print(masked_input)
+            masked_model_input = {'id': tensor([0]), 'net_input': {'audio': masked_audio_tensor, 'text': masked_text_tensor, 'video': masked_video_tensor, 'padded_amount':padded_amount_value}, 'target': tensor([0.])} #TODO change id
+            masked_model_input = utils.move_to_cuda(masked_model_input) if use_cuda else masked_model_input
             ## Collect the prediction andthe probabilities, let the model infer
             prediction, probabilities = task.valid_step(masked_input, model, criterion)
             ## Store the outcome of the model
             result[i] = (prediction, probabilities)
     return result
+
+
+
+def cli_main():
+
+    # only override args that are explicitly given on the command line
+    override_parser = options.get_validation_parser()
+    override_args = options.parse_args_and_arch(override_parser, suppress_defaults=True)
+
+    load_models(args, override_args)
+if __name__ == '__main__':
+    cli_main()
